8416911ed0cf273603370ddae195342142d47606
marcinm <marcinmat16@gmail.com>
2018-10-09 17:48:01 +0200

    create a solution based on Enum class

--- src/main/java/me/michalik/blueservice/InvestmentCalculatorImpl.java
+++ src/main/java/me/michalik/blueservice/InvestmentCalculatorImpl.java
@@ -5,8 +5,6 @@ import me.michalik.blueservice.domain.InvestmentCalculatorResult;
 import me.michalik.blueservice.domain.InvestmentStyle;
 import me.michalik.blueservice.service.DefaultDivisionCalculator;
 import me.michalik.blueservice.service.DivisionCalculator;
-import me.michalik.blueservice.service.PercentCalculator;
-import me.michalik.blueservice.service.PercentCalculatorFactory;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -29,15 +27,12 @@ public class InvestmentCalculatorImpl implements InvestmentCalculator {
         *
         * */
 
-        PercentCalculator percentCalculator = PercentCalculatorFactory.createPercentCalculator(investmentStyle);
-        if(percentCalculator==null){
-            throw new RuntimeException(""); // TODO - prepare Exception
-        }
         DivisionCalculator divisionCalculator = new DefaultDivisionCalculator();
         return funds.stream().map(fund -> {
             // W zależności od stylu inwestowania określić procent - wyrzucić sobie to w inną część (może jakaś Fabryka) - zakładamy możliwą zmiannę logiki
             // Wyliczyć kwotę dla wybranego funduszu - wyrzuć sobie do innej części - zakładamy zmianę logiki wyliczania
-            BigDecimal percent = percentCalculator.calculatePercent(fund.getType(), funds);
+
+            BigDecimal percent = investmentStyle.calculatePercent(fund.getType(), funds);
             return new InvestmentCalculatorResult(fund, divisionCalculator.calc(amountOfInvestment, percent), percent);
         }).collect(Collectors.toList());
         // TODO - walidacja przekazanych funduszy może również odbywać się tutaj poprzez sumowanie wszystkich procentów i sprawdzenie czy równają się 100%
--- src/main/java/me/michalik/blueservice/domain/InvestmentStyle.java
+++ src/main/java/me/michalik/blueservice/domain/InvestmentStyle.java
@@ -1,9 +1,60 @@
 package me.michalik.blueservice.domain;
 
+import java.math.BigDecimal;
+import java.util.Collection;
+
 public enum InvestmentStyle {
+    // Bezpieczny
+    SAFE(20, 75, 5),
+    // Zrównoważony
+    BALANCED(30, 60, 10),
+    // Agresywny
+    AGGRESSIVE(40, 20, 40);
 
-    SAFE, // Bezpieczny
-    BALANCED, // Zrównoważony
-    AGGRESSIVE // Agresywny
+    private int polishPercent;
+    private int foreignPercent;
+    private int financialPercent;
 
+    InvestmentStyle(int polishPercent, int foreignPercent, int financialPercent) {
+        this.polishPercent = polishPercent;
+        this.foreignPercent = foreignPercent;
+        this.financialPercent = financialPercent;
+    }
+
+    public int getPolishPercent() {
+        return polishPercent;
+    }
+
+    public int getForeignPercent() {
+        return foreignPercent;
+    }
+
+    public int getFinancialPercent() {
+        return financialPercent;
+    }
+
+    public BigDecimal calculatePercent(FundType fundType, Collection<Fund> funds) {
+        switch (fundType){
+            case POLISH:
+                return new BigDecimal(getPolishPercent()).divide(BigDecimal.valueOf(getNumberOfPolishFunds(funds)));
+            case FOREIGN:
+                return new BigDecimal(getForeignPercent()).divide(BigDecimal.valueOf(getNumberOfForeignFunds(funds)));
+            case FINANCIAL:
+                return new BigDecimal(getFinancialPercent()).divide(BigDecimal.valueOf(getNumberOfFinancialFunds(funds)));
+            default:
+                return BigDecimal.valueOf(0);
+        }
+    }
+
+    private long getNumberOfPolishFunds(Collection<Fund> funds){
+        return funds.stream().filter(fund -> fund.getType().equals(FundType.POLISH)).count();
+    }
+
+    private long getNumberOfForeignFunds(Collection<Fund> funds){
+        return funds.stream().filter(fund -> fund.getType().equals(FundType.FOREIGN)).count();
+    }
+
+    private long getNumberOfFinancialFunds(Collection<Fund> funds){
+        return funds.stream().filter(fund -> fund.getType().equals(FundType.FINANCIAL)).count();
+    }
 }
