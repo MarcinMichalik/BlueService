dc7b05c02bfd1fa7ad58eb039741ec2cf91f0cb3
marcinm <marcinmat16@gmail.com>
2018-10-19 07:26:50 +0200

    update Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# BlueService zadanie domowe
+# Investment calculator
+Aplikacja wykonująca automatyczny podział podanej kwoty inwestycji dla wybranego stylu inwestowania oraz fundszy.
+
+### Struktura aplikacji
+| Pakiet | Opis |
+| --- | --- |
+| me.michalik.blueservice | a |
+| me.michalik.blueservice.domain | a |
+| me.michalik.blueservice.exceptions | a |
+| me.michalik.blueservice.services | a |
+
+### Wymagania
+Do poprawnego działania aplikacji wymagana jest Java w wersji minimum 8 (Java 8+).
+
+### Testowanie aplikacji
+```bash
+./gradlew test
+```
--- src/test/java/me/michalik/blueservice/InvestmentCalculatorImplTest.java
+++ src/test/java/me/michalik/blueservice/InvestmentCalculatorImplTest.java
@@ -92,6 +92,12 @@ public class InvestmentCalculatorImplTest {
         expectedEx.expect(InsufficientAmountOfInvestmentException.class);
         InvestmentCalculator investmentCalculator = new InvestmentCalculatorImpl();
         Set<Fund> funds = new HashSet<>();
+        funds.add(new Fund(1L, "Fundusz Polski 1", FundType.POLISH));
+        funds.add(new Fund(2L, "Fundusz Polski 2", FundType.POLISH));
+        funds.add(new Fund(3L, "Fundusz Zagraniczny 1", FundType.FOREIGN));
+        funds.add(new Fund(4L, "Fundusz Zagraniczny 2", FundType.FOREIGN));
+        funds.add(new Fund(5L, "Fundusz Zagraniczny 3", FundType.FOREIGN));
+        funds.add(new Fund(6L, "Fundusz Pieniężny 1", FundType.FINANCIAL));
         investmentCalculator.calculate(new BigDecimal(0), InvestmentStyle.BALANCED, funds);
     }
 
